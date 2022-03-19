fe85b3a69bceea29e1c8f5a695b0b7fdd5bf155a
Dutt23 <shatyaki_dutt@hotmail.com>
2022-03-19 14:52:03 +0530

    Handle signin route, and change password logic

--- auth/src/services/password.ts
+++ auth/src/services/password.ts
@@ -8,13 +8,17 @@ export class Password{
 
   static async toHash(password: string): Promise<string> {
     const salt = randomBytes(8).toString('hex');
-    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+    return this.toHashWithSalt(password, salt)
+  }
 
+  static async toHashWithSalt(password: string, salt: string): Promise<string> {
+    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
     return `${buf.toString('hex')}.${salt}`
   }
 
   static async compare(storedPassword: string, suppliedPassword: string): Promise<boolean> {
-    const hashedPassword = await Password.toHash(suppliedPassword);
+    const [, salt] = storedPassword.split('.');
+    const hashedPassword = await this.toHashWithSalt(suppliedPassword,salt );
     return hashedPassword === storedPassword;
   }
 
