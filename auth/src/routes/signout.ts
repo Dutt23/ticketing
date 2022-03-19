ea1ae73eaa4c80ea9d1b725126e7d37dabb32226
Dutt23 <shatyaki_dutt@hotmail.com>
2022-03-19 15:31:16 +0530

    signout handler

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,11 +3,8 @@ import express from 'express'
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) =>{
-  res.send({
-    user: 1,
-    email: 'shatyaki_dutt@hotmail.com',
-    message: 'success'
-  })
+  req.session = null;
+  res.send({})
 })
 
 export { router as signoutRouter}
