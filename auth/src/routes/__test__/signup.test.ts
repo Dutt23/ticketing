e10e3b43cbb59425353dbfb102fa7b72f0ac216e
Dutt23 <shatyaki_dutt@hotmail.com>
2022-03-25 17:25:54 +0530

    Add tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,43 @@ it('returns a 201 with successfull response', async () =>{
          })
         .expect(201)
 })
+
+it('should return 400 for missing password', async () =>{
+  return request(app)
+         .post('/api/users/signup')
+         .send({
+           email:'test@test.com',
+           password: ''
+         })
+        .expect(400)
+})
+
+it('should return 400 for missing email', async () =>{
+  return request(app)
+         .post('/api/users/signup')
+         .send({
+           email:'',
+           password: '1234567'
+         })
+        .expect(400)
+})
+
+it('should return 400 for invalid password', async () =>{
+  return request(app)
+         .post('/api/users/signup')
+         .send({
+           email:'test@test.com',
+           password: '123'
+         })
+        .expect(400)
+})
+
+it('should return 400 for invalid email', async () =>{
+  return request(app)
+         .post('/api/users/signup')
+         .send({
+           email:'testtest.com',
+           password: '1234567'
+         })
+        .expect(400)
+})
