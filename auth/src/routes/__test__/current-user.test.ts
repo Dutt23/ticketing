e744b958671742f4fd873af1298b35533d3a3ec3
Dutt23 <shatyaki_dutt@hotmail.com>
2022-03-25 18:34:34 +0530

    un authenticated test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('Should respond with details of the current user', async () =>{
-const cookie = await global.signin()
+const cookie  = await global.signin()
 const response = await request(app)
                       .get('/api/users/currentUser')
                       .set('Cookie', cookie)
@@ -14,3 +14,15 @@ const response = await request(app)
 
 expect(response.body.currentUser.email).toEqual('test@test.com')
 })
+
+it('Should respond with details of the current user as null', async () =>{
+  const response = await request(app)
+                        .get('/api/users/currentUser')
+                        .send({
+                          email:'test@test.com',
+                          password: 'password'
+                        })
+                        .expect(200)
+  
+  expect(response.body.currentUser).toEqual(null)
+  })
