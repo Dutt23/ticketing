f29952c458e8dfb6c0a8c70587d13b422f4459ba
Dutt23 <shatyaki_dutt@hotmail.com>
2022-04-10 13:55:38 +0530

    Render errors

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -5,6 +5,7 @@ const SignUp = () => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
   const onSubmit = async (event) =>{
     event.preventDefault();
@@ -12,11 +13,8 @@ const SignUp = () => {
       const response = await axios.post('/api/users/signup',{
         email, password
       })
-      
-      console.log(response.data)
     } catch (err){
-      console.log("ISSISJI")
-      console.log(err.response)
+      setErrors(err.response.data.errors)
     }
   }
 
@@ -38,6 +36,17 @@ const SignUp = () => {
           type="password" 
           className="form-control"/>
       </div>
+      {errors.length > 0}
+      {
+        errors.length > 0 && (
+          <div className='alert alert-danger'>
+          <h4>Opps....</h4>
+          <ul className='my-0'>
+          {errors.map(err => <li key={err.message}> {err.message}</li>)}
+          </ul>
+          </div>
+        )
+      }
       <button className="btn btn-primary">Sign Up</button>
     </form>
   )
