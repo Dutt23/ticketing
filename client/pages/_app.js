e0994cab199e074e35481fa3db280ed435553cf1
Dutt23 <shatyaki_dutt@hotmail.com>
2022-05-08 14:59:38 +0530

    Handle dynamic links

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import buildClient from '../api/build-client';
-
+import Header from '../components/header';
 // When any page is rendered in next.js, it does not just render the component
 // It wraps in it's own component called _.app.js
 // Here we are making out own _app.js, to handle global css
@@ -9,7 +9,7 @@ const AppComponent = ({ Component, pageProps, currentUser}) =>{
   console.log(Component)
   return (
     <div>
-      <h1>Header ! {currentUser.email}</h1>
+      <Header currentUser={currentUser}/>
       <Component {...pageProps} currentUser={currentUser} />
     </div>
   )
