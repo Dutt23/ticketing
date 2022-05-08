8050a564b1ea477a8cac3c59569c701e519c653a
Dutt23 <shatyaki_dutt@hotmail.com>
2022-05-08 14:39:33 +0530

    Fetch current user in root component

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,10 +1,32 @@
 import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/build-client';
 
 // When any page is rendered in next.js, it does not just render the component
 // It wraps in it's own component called _.app.js
 // Here we are making out own _app.js, to handle global css
-export default ({ Component, pageProps}) =>{
+const AppComponent = ({ Component, pageProps, currentUser}) =>{
   console.log("Component passed");
   console.log(Component)
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <h1>Header ! {currentUser.email}</h1>
+      <Component {...pageProps} currentUser={currentUser} />
+    </div>
+  )
 }
+
+// https://github.com/vercel/next.js/blob/deprecated-main/errors/circular-structure.md
+AppComponent.getInitialProps = async (context) =>{
+  const { data } = await buildClient(context.ctx).get('/api/users/currentUser');
+  let pageProps = {};
+  if(context?.Component?.getInitialProps) {
+    pageProps = await context.Component.getInitialProps(context.ctx);
+  }
+  return {
+    pageProps,
+    currentUser: data.currentUser 
+  };
+}
+
+export default AppComponent;
+
--- client/pages/index.js
+++ client/pages/index.js
@@ -7,12 +7,4 @@ const Landing = ({
   return <h1>{content}</h1>
 }
 
-
-// https://github.com/vercel/next.js/blob/deprecated-main/errors/circular-structure.md
-Landing.getInitialProps = async (context) =>{
-  const { data } = await buildClient(context).get('/api/users/currentUser');
-
-  return data;
-}
-
 export default Landing;
