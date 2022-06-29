2a56423e25c9251b82f14aaade32449bb57e69c2
Dutt23 <shatyaki_dutt@hotmail.com>
2022-06-29 19:45:56 +0530

    Updates

--- common/src/index.ts
+++ common/src/index.ts
@@ -4,6 +4,7 @@ export * from './errors/database-connection-errror';
 export * from './errors/not-authorized-error';
 export * from './errors/request-validation-errors';
 export * from './errors/route-not-found-error';
+export * from './errors/not-found-error';
 
 export * from './middlewares/current-user';
 export * from './middlewares/error-handler';
