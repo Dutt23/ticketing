9c7c242c201cd84207f6deb609a6e15a5336162d
Dutt23 <shatyaki_dutt@hotmail.com>
2022-06-29 17:28:17 +0530

    Add tests

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -19,17 +19,49 @@ it('return status other than 401 if signed in', async () =>{
   const cookie = global.signin();
   const response = await request(app)
     .post('/api/tickets')
-    .set('cookie', cookie[0])
+    .set('Cookie', cookie)
     .send({});
   expect(response.statusCode).not.toEqual(401);
 });
 
 it('return error if invalid title provided', async () =>{
-  
+  const cookie = global.signin();
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', cookie)
+  .send({
+    title:'',
+    price: 10
+  })
+  .expect(400);
+
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', cookie)
+  .send({
+    price: 10
+  })
+  .expect(400);
 });
 
 it('return error if invalid price is provided', async () =>{
-  
+  const cookie = global.signin();
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', cookie)
+  .send({
+    title:'test',
+    price: -10
+  })
+  .expect(400);
+
+  await request(app)
+  .post('/api/tickets')
+  .set('Cookie', cookie)
+  .send({
+    title:'test',
+  })
+  .expect(400);
 });
 
 it('creates a ticket with valid parameters', async () =>{
